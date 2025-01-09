e9f38b3f6b97d7a391bfb81b35a5a596a4a19208
Katsuya Komai <komai_katsuya@uniquevision.co.jp>
2025-01-09 15:14:03 +0900

    chore(webpack): comment out Webpack installation steps in install script

--- lib/install/webpack/install.rb
+++ lib/install/webpack/install.rb
@@ -1,9 +1,9 @@
 apply "#{__dir__}/../install.rb"
 apply "#{__dir__}/../install_procfile.rb"
 
-say "Install Webpack with config"
-copy_file "#{__dir__}/webpack.config.js", "webpack.config.js"
-run "yarn add --dev webpack webpack-cli@4.10.0"
+# say "Install Webpack with config"
+# copy_file "#{__dir__}/webpack.config.js", "webpack.config.js"
+# run "yarn add --dev webpack webpack-cli@4.10.0"
 
 say "Add build script"
 build_script = "webpack --config ./config/webpack/webpack.config.js"
