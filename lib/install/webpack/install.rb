e409be27400789c0d8109266d56ec40aa96911cc
Katsuya Komai <komai_katsuya@uniquevision.co.jp>
2025-01-09 13:47:12 +0900

    refactor(webpack): simplify script addition by removing version-specific logic for `npx`

--- lib/install/webpack/install.rb
+++ lib/install/webpack/install.rb
@@ -7,14 +7,15 @@ run "yarn add --dev webpack webpack-cli@4.10.0"
 
 say "Add build script"
 build_script = "webpack --config webpack.config.js"
+say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 
-case `npx -v`.to_f
-when 7.1...8.0
-  run %(npm set-script build "#{build_script}")
-  run %(yarn build)
-when (8.0..)
-  run %(npm pkg set scripts.build="#{build_script}")
-  run %(yarn build)
-else
-  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
-end
+# case `npx -v`.to_f
+# when 7.1...8.0
+#   run %(npm set-script build "#{build_script}")
+#   run %(yarn build)
+# when (8.0..)
+#   run %(npm pkg set scripts.build="#{build_script}")
+#   run %(yarn build)
+# else
+#   say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
+# end
